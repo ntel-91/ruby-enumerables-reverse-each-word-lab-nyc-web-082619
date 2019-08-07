1bbaa2ca6638a55ff38094f6ab0370d2efd776d9
Nick Telenson <ntelenson@gmail.com>
2019-08-07 14:57:25 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,6 @@
 def reverse_each_word (string)
-  string.each{ |i| p i.reverse }
+  
+  string.split.each{ |i| p i.reverse }
 end
-
-p "Hello there, this is Nick".split(" ")
-
 
 p reverse_each_word(["Hello", "there", "today"])
